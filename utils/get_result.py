61b78dd255bd04b429aaf19de7c24a0352f2ba6a
chwangthu <chwangthu@gmail.com>
2019-04-20 23:37:28 +0800

    update util

--- utils/get_result.py
+++ utils/get_result.py
@@ -1,23 +1,33 @@
 import os
 import re
+import csv
+import datetime
 import gzip
 
 def file_name(file_dir):
     for _, _, files in os.walk(file_dir):
             return files
 
-def extract_res(file):
-    f = gzip.open(file, 'rb')
+def extract_res(path, file):
+    f = gzip.open(path+file, 'rb')
     content = f.read()
     content = content.decode('utf-8')
     res_pattern = re.compile(u".*Thread:.*Lookups.*\n", re.IGNORECASE)
     res = re.findall(res_pattern, content)
-    print(file, res)
+    # print(file[0:3], res[0][:-1].split(" ")[-1])
     f.close()
+    return file[0:3], res[0][:-1].split(" ")[-1]
 
 if __name__ == "__main__":
     path="../result/"
+    result = {}
     files = file_name(path)
     for item in files:
         if item[-8:] == "stats.gz":
-            extract_res(path+item)
+            name, res = extract_res(path, item)
+            result[int(name)] = round(float(res),3) 
+    print(result)
+    with open(str(datetime.datetime.now())+".csv", "w") as f:
+        writer = csv.writer(f)
+        for item in result:
+            writer.writerow([item, result[item]])
--- utils/list.sh
+++ utils/list.sh
@@ -6,7 +6,7 @@ do
     end=${filename:${#filename}-8}
     if [ "$end" = "trace.gz" ]; then
 #    echo ${filename:0:${#filename}-9}.stats
-    ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/$filename -o ../result/${filename:0:${#filename}-9}.stats -cache UL3:1024:64:16 -LLCrepl 0
+    ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/$filename -o ../result/${filename:0:${#filename}-9}.stats -cache UL3:1024:64:16 -LLCrepl 1
     fi
 done
 
